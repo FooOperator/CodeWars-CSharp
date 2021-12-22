34b3caf16bf8187569473f62672f355838db3d4f
devgcard@gmail.com <devgcard@gmail.com>
2021-12-22 16:05:36 +0000

    Removed TestGreedGame method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using Flee;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Codewars_Sharp
 {
@@ -7,19 +6,8 @@ namespace Codewars_Sharp
     {
         public static void Main(string[] args)
         {
-            TestGreedGame();
+            GreedGame.TestGreedGame();
         }
 
-        private static void TestGreedGame()
-        {
-            int[] dice1 = { 5, 1, 3, 4, 1 };
-            int[] dice2 = { 6, 6, 6, 6, 6, 6, 3, 3, 3, 3, 3 };
-            int[] dice3 = { 1, 1, 1, 1, 3 };
-            int[] dice4 = { 4, 4, 4, 3, 3 };
-            int[] dice5 = { 5, 5, 5, 5, 2 };
-            int[] finalDice = { 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 2, 2, 2, 3, 3 };
-            var result = GreedGame.Score(dice3);
-            Console.WriteLine(result);
-        }
     }
 }

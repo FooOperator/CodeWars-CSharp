55eaed56670ebc411fe5b201e79d1303c337448a
devgcard@gmail.com <devgcard@gmail.com>
2021-12-22 15:10:02 +0000

    Add project files.

---
+++ GreedGame.cs
@@ -0,0 +1,97 @@
+﻿using Flee.PublicTypes;
+
+namespace Codewars_Sharp
+{
+    public class GreedGame
+    {
+        static ExpressionContext context = new ExpressionContext();
+        static int[] acceptableRange = { 2, 3, 4, 6 };
+
+        public static int Score(int[] dice)
+        {
+            int score = 0;
+            string equation = "";
+
+            Dictionary<int, int> diceCount = new Dictionary<int, int>();
+
+            foreach (int die in dice)
+            {
+                if (!diceCount.ContainsKey(die))
+                {
+                    diceCount.Add(die, 0);
+                }
+                diceCount[die] += 1;
+            }
+
+            foreach (KeyValuePair<int, int> keyValue in diceCount)
+            {
+                Console.WriteLine($"{keyValue.Key} appears {keyValue.Value} times");
+
+                var dieScore = DefineDieScore(keyValue);
+                if (dieScore == null) continue;
+                dieScore = string.Concat(dieScore, " + ");
+                equation = string.Concat(equation, dieScore);
+
+            }
+
+            if (equation.EndsWith(" + "))
+            {
+                equation = equation.Substring(0, equation.Length - 3);
+            }
+
+            Console.WriteLine(equation);
+            if (equation != "")
+            {
+                IDynamicExpression e = context.CompileDynamic(equation);
+
+                score = (int)e.Evaluate();
+
+            }
+
+            return score;
+        }
+        private static string? DefineDieScore(KeyValuePair<int, int> keyValue)
+        {
+            Func<int, bool> than3 = value => value >= 3;
+            int howManyPairs = keyValue.Value / 3;
+            string calculation = $"({keyValue.Key * 100} * {keyValue.Value / 3})";
+
+            if (acceptableRange.Contains(keyValue.Key))
+            {
+                if (than3(keyValue.Value))
+                {
+                    return calculation;
+                }
+            }
+            else if (keyValue.Key == 5)
+            {
+                calculation = $"({keyValue.Key * 10} * {keyValue.Value - (howManyPairs * 3)})";
+                if (than3(keyValue.Value))
+                {
+                    int times100 = 0;
+                    foreach (int pair in Enumerable.Range(0, howManyPairs))
+                    {
+                        times100++;
+                    }
+                    calculation = string.Concat(calculation, $" + ({(keyValue.Key * 100) * times100})");
+                }
+                return calculation;
+            }
+            else if (keyValue.Key == 1)
+            {
+                calculation = $"({keyValue.Key * 100} * {keyValue.Value - (howManyPairs * 3)})";
+                if (than3(keyValue.Value))
+                {
+                    int times1000 = 0;
+                    foreach (int pair in Enumerable.Range(0, howManyPairs))
+                    {
+                        times1000++;
+                    }
+                    calculation = string.Concat(calculation, $" + ({(keyValue.Key * 1000) * times1000})");
+                }
+                return calculation;
+            }
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Flee;
+using System.Linq;
+
+namespace Codewars_Sharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestGreedGame();
+        }
+
+        private static void TestGreedGame()
+        {
+            int[] dice1 = { 5, 1, 3, 4, 1 };
+            int[] dice2 = { 6, 6, 6, 6, 6, 6, 3, 3, 3, 3, 3 };
+            int[] dice3 = { 1, 1, 1, 1, 3 };
+            int[] dice4 = { 4, 4, 4, 3, 3 };
+            int[] dice5 = { 5, 5, 5, 5, 2 };
+            int[] finalDice = { 1, 1, 1, 1, 1, 1, 1, 5, 5, 5, 5, 2, 2, 2, 3, 3 };
+            var result = GreedGame.Score(dice3);
+            Console.WriteLine(result);
+        }
+    }
+}
